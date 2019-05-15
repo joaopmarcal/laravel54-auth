85e70e90980a42d4b2430308e3827e5dbc3ad34e
João Paulo <paulospc21@gmail.com>
2019-05-15 08:50:40 -0300

    autenticação e múltiplas áreas

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin'], function (){
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function (){
 
     // Authentication Routes...
     $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -31,5 +31,7 @@ Route::group(['prefix' => 'admin'], function (){
     $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
     $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
     $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+    Route::get('/home', 'HomeController@index')->name('home');
 
 });
